7f9d8555df334bff1dbe44fc20c9869231842ebd
ahmed <ahmedgf100@gmail.com>
2018-04-14 00:56:46 +0200

    Finish Background Service

--- app/src/main/java/abass/com/firebasepushnotifications/MyBackgroundService.java
+++ app/src/main/java/abass/com/firebasepushnotifications/MyBackgroundService.java
@@ -149,7 +149,7 @@ public class MyBackgroundService extends Service implements ConnectivityReceiver
 
         // Create the location request to start receiving updates
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         mLocationRequest.setInterval(UPDATE_INTERVAL);
         mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
