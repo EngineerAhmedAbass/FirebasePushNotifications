f2ef4a8f13a4785b90a3f2d7fcba51a294561314
ahmed <ahmedgf100@gmail.com>
2018-04-14 15:02:57 +0200

    Finish Background Service

--- app/src/main/java/abass/com/firebasepushnotifications/Request/bloodDonationRequest.java
+++ app/src/main/java/abass/com/firebasepushnotifications/Request/bloodDonationRequest.java
@@ -216,7 +216,7 @@ public class bloodDonationRequest extends AppCompatActivity {
         });
         Message = requestText.getText().toString();
         btype = spinner.getSelectedItem().toString();
-        Message +="\n";
+        Message +=" \n ";
         Message += btype + "فصيلة الدم "  ;
         if (Message.equals("")) {
             Toast.makeText(bloodDonationRequest.this, "من فضلك ادخل معلومات عن طلب المساعدة", Toast.LENGTH_SHORT).show();
